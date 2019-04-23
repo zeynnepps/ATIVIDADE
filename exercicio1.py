e25f403d37642d6d909c24167fa40c3fc1dbbd0f
Zeynep <zeynepsalihoglu@gmail.com>
2019-04-23 12:11:31 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,27 @@
+A1 = float(input('Digite nota A1:'))
+P1 = float(input('Digite nota P1:'))
+A2 = float(input('Digite nota A2:'))
+P2 = float(input('Digite nota P2:'))
+
+
+def notas(A1, P1, A2, P2):
+    peso_A1=2
+    peso_P1=4
+    peso_A2=3
+    peso_P2=3
+    
+   
+
+print('A1=', A1, 'P1=', P1, 'A2=', A2, 'P2=', P2)
+
+def MF(M1, M2, M3, M4):
+    M1 = A1*2
+    M2 = P1*4
+    M3 = A2*3
+    M4 = P2*3
+    MF = (M1 + M2 + M3 + M4)/10
+    return MF
+
+
+    
+
